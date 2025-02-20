6c4e686cc94fdd1dd91e5822ed300fe8a987b635
Allegra <allegraruthedimo@gmail.com>
2025-02-20 09:29:04 +0100

    ajout des modifications sur les fichiers suite à la fin de la partie 3: gérez des données à l'aide du site d'administration de django

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,41 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    
+    class Genre(models.TextChoices):
+        Hip_Hop = "HH"
+        SYNTH_POP = "SP"
+        ALTERNATIVE_ROCK = "AR"
+        REGGAE = "RG"
+        COUNTRY = "CT"
+    
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biograpphy = models.fields.CharField(max_length=100)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Listing(models.Model):
+    
+    class Type(models.TextChoices):
+        RECORDS = "REC"
+        CLOTHING = "CLO"
+        POSTERS = "POS"
+        MISCELLANEOUS = "MIS"
+    
+    def __str__(self):
+        return f'{self.title}'
+
     title = models.fields.CharField(max_length=100)
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
+    description = models.fields.CharField(max_length=100)
+    sold = models.fields.BooleanField(default=True)
+    year = models.fields.IntegerField(null=True, blank=True)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+    
