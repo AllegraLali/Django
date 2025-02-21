48b6034c55873cc2dd487dcfd89cb47719f1fbd6
Allegra <allegraruthedimo@gmail.com>
2025-02-21 10:24:27 +0100

    ajout des modifications sur les fichiers suite à la fin des deux premières lecons de la partie 4

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,18 +2,43 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band
 from listings.models import Listing
+from listings.forms import ContactUsForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, "listings/hello.html", {"bands": bands})
+    return render(request, "listings/band_list.html", {"bands": bands})
+
+def band_detail(request, band_id):
+    band = Band.objects.get(id=band_id)
+    return render(request, "listings/band_detail.html", {"band":band})
 
 def about(request):
     return render(request, "listings/about.html")
 
-def listings(request):
-    lists = Listing.objects.all()
-    return render(request, "listings/listings.html", {"lists": lists})
+def listing_list(request):
+    listings = Listing.objects.all()
+    return render(request, "listings/listing_list.html", {"listings": listings})
+
+def listing_detail(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    return render(request, "listings/listing_detail.html", {"listing": listing})
 
 def contact(request):
-    return render(request, "listings/contact.html")
+    
+    if request.method == "POST":
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via Merchex Contact Us form',
+                message=form.cleaned_data["message"],
+                from_email=form.cleaned_data["email"],
+                recipient_list=["admin@merchex.xyz"]
+            )
+    else:
+        form = ContactUsForm()
+
+    return redirect("email-sent")
