57666651a7c8cc903c0ebc35b00485d307253bc6
Allegra <allegraruthedimo@gmail.com>
2025-02-22 09:36:09 +0100

    ajout des modifications sur les fichiers suite à la fin de la troisième lecon de la partie 4

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band
 from listings.models import Listing
-from listings.forms import ContactUsForm
+from listings.forms import BandForm, ContactUsForm, ListingForm
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -15,6 +15,16 @@ def band_detail(request, band_id):
     band = Band.objects.get(id=band_id)
     return render(request, "listings/band_detail.html", {"band":band})
 
+def band_create(request):
+    if request.method == "POST":
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect("band-detail", band.id)
+    else:
+        form = BandForm()
+    return render(request, "listings/band_create.html", {"form": form})
+
 def about(request):
     return render(request, "listings/about.html")
 
@@ -25,6 +35,17 @@ def listing_list(request):
 def listing_detail(request, listing_id):
     listing = Listing.objects.get(id=listing_id)
     return render(request, "listings/listing_detail.html", {"listing": listing})
+
+def listing_create(request):
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect("listing-detail", listing.id)
+    else:
+        form = ListingForm()
+    return render(request, "listings/listing_create.html", {"form": form})
+
 
 def contact(request):
     
@@ -38,7 +59,10 @@ def contact(request):
                 from_email=form.cleaned_data["email"],
                 recipient_list=["admin@merchex.xyz"]
             )
+        return redirect("email-sent")
     else:
         form = ContactUsForm()
+    return render(request, "listings/contact.html", {"form": form})
 
-    return redirect("email-sent")
+def email_sent(request):
+    return render(request, "listings/email_sent.html")
